020cf5399ecdb1c14b59d590f3b124a5b401c959
Ischi94 <gmathes94@gmx.de>
2021-01-18 15:17:56 +0100

    update perc_change

--- R/perc_change.R
+++ R/perc_change.R
@@ -1,25 +1,26 @@
-#' Calculate percentage change
+#'Calculate percentage change
 #'
-#'\code{perc_change} returns the percentage change between two values, where one value (\code{old.value}) is
-#'used as a baseline to compare the change with.
+#'\code{perc_change} returns the percentage change between two values, where one
+#'value (\code{old.value}) is used as a baseline to compare the change with.
 #'
-#'The output can be specified via the \code{print.result} parameter.
-#'Options are the raw value, transformed to percentage, or a text description.
-#'Default settings return the raw value.
+#'The output can be specified via the \code{print.result} parameter. Options are
+#'the raw value, transformed to percentage, or a text description. Default
+#'settings return the raw value.
 #'
-#'@section Warning:
-#'\code{perc_change} is vectorized and shorter values are recycled.
-#'If the length of \code{old.value} differs from the length of
-#'\code{print.result}, please confirm that the code is doing the desired task by double-checking with
-#'print.result = "text".
+#'@section Warning: \code{perc_change} is vectorized and shorter values are
+#'  recycled. If the length of \code{old.value} differs from the length of
+#'  \code{print.result}, please confirm that the code is doing the desired task
+#'  by double-checking with print.result = "text".
 #'
-#' @param old.value A numeric value or vector. This is the baseline for the comparison.
-#' @param new.value A numeric value or vector. This value is compared to the baseline.
-#' @param print.result Character. Either "value", "percentage", or "text".
+#'@param old.value A numeric value or vector. This is the baseline for the
+#'  comparison.
+#'@param new.value A numeric value or vector. This value is compared to the
+#'  baseline.
+#'@param print.result Character. Either "value", "percentage", or "text".
 #'
-#' @export
+#'@export
 #'
-#' @examples
+#'@examples
 #' perc_change(7, 5, print.result = "text")
 perc_change <- function(old.value, new.value, print.result = "value"){
   if(!is.numeric(old.value) || !is.numeric(new.value)){
