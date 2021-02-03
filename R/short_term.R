af50bea65a98b4d17195932391b28a287639984b
Ischi94 <gmathes94@gmx.de>
2021-02-03 15:00:11 +0100

    clean code for mult obs

--- R/short_term.R
+++ R/short_term.R
@@ -99,7 +99,9 @@ short_term <- function(data, value, bin, bin.one = "oldest", mult.observations =
       tidyr::nest()
 
     suppressMessages(
-      output <- data.mult %>%
+      output <- data %>%
+        dplyr::group_by({{ bin }}) %>%
+        tidyr::nest() %>%
         dplyr::mutate(val.com = purrr::map(data, rlang::as_label(rlang::enquo(value)))) %>%
         dplyr::ungroup() %>%
         dplyr::select(val.com, {{ bin }}) %>%
@@ -113,16 +115,19 @@ short_term <- function(data, value, bin, bin.one = "oldest", mult.observations =
           comb.bin = list(rep(c({{ bin }}, lag.bin), each = 2))
         ) %>%
         tidyr::drop_na() %>%
-        dplyr::select({{ bin }}, comb.val, comb.bin) %>%
-        tidyr::nest(data = c(comb.val, comb.bin)) %>%
-        dplyr::mutate(
-          model = list(stats::lm(comb.val[[1]] ~ comb.bin[[1]], data = data[[1]])),
-          short_term = purrr::map(model, "coefficients"),
-          short_term = purrr::map_dbl(short_term, purrr::pluck, 2)
-        ) %>%
-        dplyr::select({{ bin }}, short_term) %>%
         dplyr::ungroup() %>%
-        dplyr::full_join(data.mult)
+        dplyr::select({{ bin }}, comb.val, comb.bin) %>%
+        tidyr::unnest(cols = c(comb.val, comb.bin)) %>%
+        dplyr::nest_by({{ bin }}) %>%
+        dplyr::mutate(model = list(lm(comb.val ~ comb.bin, data = data))) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(
+          short_term = purrr::map(model, "coefficients"),
+          short_term = purrr::map_dbl(short_term, purrr::pluck, 2)) %>%
+        dplyr::select({{ bin }}, short_term) %>%
+        dplyr::full_join(data.mult) %>%
+        dplyr::select({{ value }} := data, {{ bin }}, short_term) %>%
+        dplyr::arrange({{ bin }})
     )
   }
 
