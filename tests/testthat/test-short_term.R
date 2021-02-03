785299a3ade1f5e38b4e74e9a30a3e0030fdbc7b
Ischi94 <gmathes94@gmx.de>
2021-02-03 10:09:37 +0100

    add test for mult observations

--- tests/testthat/test-short_term.R
+++ tests/testthat/test-short_term.R
@@ -15,6 +15,8 @@ test_that("the change from 10 to 12 is minus two, when the first value is younge
   )[[2, 3]], -2)
 })
 
+
+
 test_that("when there is no previous bin, NA is returned", {
   expect_equal(short_term(
     data = data.frame(x = c(10, 12), stg = c(1, 2)),
@@ -41,7 +43,7 @@ test_that("the output has the same length as unique stages", {
   )), length(unique(rep(1:10, 2))))
 })
 
-test_that("the output is a tibble for all cases", {
+test_that("the output is a tibble for default", {
   expect_equal(tibble::is_tibble(short_term(
     data = data.frame(x = c(1:20), stg = rep(1:10, 2)),
     value = x,
@@ -53,6 +55,23 @@ test_that("the output is a tibble for all cases", {
     data = data.frame(x = c(10, 12), stg = c(1, 2)),
     value = x,
     bin = stg
+  )), TRUE)
+})
+
+test_that("the output is a vector for print.result = 'vector'", {
+  expect_equal(is.vector(short_term(
+    data = data.frame(x = c(1:20), stg = rep(1:10, 2)),
+    value = x,
+    bin = stg,
+    mult.observations = TRUE,
+    print.result = 'vector'
+  )), TRUE)
+
+  expect_equal(tibble::is.vector(short_term(
+    data = data.frame(x = c(10, 12), stg = c(1, 2)),
+    value = x,
+    bin = stg,
+    print.result = 'vector'
   )), TRUE)
 })
 
